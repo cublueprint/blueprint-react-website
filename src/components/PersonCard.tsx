7975a726eb0079fb9910ebff5a3bc61849361ff6
seneli <seneli.sene@gmail.com>
2022-11-08 01:59:22 -0500

    styled person card

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -13,12 +13,9 @@ interface PersonProps {
 const PersonCard = ({ props }: PersonProps) => {
   return (
     <CardDiv>
-      <ProfilePicture
-        src={props.name}
-        alt={`${props.name}'s profile picture`}
-      />
-      <MemberName>{props.name}</MemberName>
-      <MemberPosition>{props.position}</MemberPosition>
+      <Picture src={props.name} alt={`${props.name}'s profile picture`} />
+      <Name>{props.name}</Name>
+      <Role>{props.position}</Role>
       <a href={props.linkedin}>
         <LinkedinIcon
           src={LinkedinLogo}
@@ -36,30 +33,41 @@ const CardDiv = styled.div`
   width: 250px;
   height: 270px;
   display: grid;
-  grid-template-columns: 4fr 1fr;
-  grid-template-rows: 5fr 1fr 1fr;
-  grid-template: 'Picture' 'Picture' 'Picture';
+  grid-template-rows: 6fr 1fr 1fr;
+  grid-template-columns: 6fr 1fr;
 `;
 
-const ProfilePicture = styled.img`
-  background-color: red;
+const Picture = styled.img`
+  grid-column-start: 1;
+  grid-column-end: 3;
   width: 100%;
   height: 200px;
   border-radius: 10px;
+  background-color: white; 
 `;
 
-const MemberName = styled.h2`
-  background-color: darkgray;
+const Name = styled.h2`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-area: 'Name';
   margin: 0;
+  color: ${(props) => props.theme.colors.primaryBlue};
+  font-weight: 700;
 `;
 
-const MemberPosition = styled.h2`
-  background-color: lightgray;
+const Role = styled.h4`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-area: 'Role';
   margin: 0;
 `;
 
 const LinkedinIcon = styled.img`
-  background-color: blue;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  grid-area: 'LinkedinIcon';
+  background-color: darkgray;
+  width: 30px;
 `;
 
 export default PersonCard;
