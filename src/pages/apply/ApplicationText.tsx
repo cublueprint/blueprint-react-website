fa1696d74835404564a826bb3dd53281ab50ec6c
seneli <seneli.sene@gmail.com>
2023-02-15 10:03:39 -0500

    apply page - page 1

---
+++ src/pages/apply/ApplicationText.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+interface ApplicationTextProps {
+  title: string;
+  body: string;
+  url: string;
+  linkText: string;
+}
+
+const ApplicationText = ({
+  title,
+  body,
+  url,
+  linkText,
+}: ApplicationTextProps) => (
+  <Container>
+    <Title>{title}</Title>
+    <Body>{body}</Body>
+    <Link href={url}>{linkText}</Link>
+  </Container>
+);
+
+const Container = styled.div`
+  width: 40vw;
+`;
+
+const Title = styled.h2`
+  color: ${(props) => props.theme.colors.primaryBlue};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-style: bold;
+`;
+
+const Body = styled.p``;
+
+const Link = styled.a`
+  color: ${(props) => props.theme.colors.primaryBlue};
+  font-style: bold;
+  text-decoration: none;
+`;
+
+export default ApplicationText;
--- src/pages/apply/Apply.tsx
+++ src/pages/apply/Apply.tsx
@@ -1,9 +1,82 @@
+import styled from 'styled-components';
+import blueprintIcon from '../../static/images/blueprint.png';
+import ApplicationText from './ApplicationText';
+
 const Apply = () => {
   return (
-    <div>
-      Join us!
-    </div>
+    <Container>
+      <TitleContainer>
+        <StyledBlueprintImage alt='Blueprint' src={blueprintIcon} />
+        <Title>blueprint</Title>
+        <Subtitle>Come work with us!</Subtitle>
+      </TitleContainer>
+
+      <BodyContainer>
+        <h2>I am a:</h2>
+        <ApplicationTextContainer>
+          <ApplicationText
+            title={'Non Profit'}
+            body={
+              'Looking for a group of slick, volunteer student developers to help with my software needs'
+            }
+            url={'#'}
+            linkText={'Learn more!'}
+          />
+          <ApplicationText
+            title={'Student'}
+            body={
+              'Looking to develop my skills and contribute to my community with like-minded nerds lmao'
+            }
+            url={'#'}
+            linkText={'Learn more!'}
+          />
+        </ApplicationTextContainer>
+      </BodyContainer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  margin: auto;
+  width: 80vw;
+  padding: 50px 0;
+`;
+
+const TitleContainer = styled.div`
+  display: grid;
+  grid-template-areas:
+    'Logo Title'
+    'Logo Subtitle';
+`;
+
+const StyledBlueprintImage = styled.img`
+  grid-area: Logo;
+  max-width: 150px;
+  margin-left: 4vw;
+  height: 100%;
+`;
+
+const Title = styled.h1`
+  grid-area: Title;
+  margin: 0;
+  color: ${(props) => props.theme.colors.primaryBlue};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-style: bold;
+  font-size: 10vw;
+`;
+
+const Subtitle = styled.p`
+  grid-area: Subtitle;
+  margin: 0;
+`;
+
+const BodyContainer = styled.div`
+  padding: 5vh 0;
+`;
+
+const ApplicationTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 export default Apply;
