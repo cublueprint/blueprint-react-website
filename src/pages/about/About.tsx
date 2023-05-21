5c01c75d8c4335b25c2fa8093967d5b7cf233994
heyitsOK <42219936+heyitsOK@users.noreply.github.com>
2023-05-20 22:41:39 -0400

    Added title block on about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,21 @@
+import TitleBlock from '../../components/TitleBlock';
+
+//import styled from 'styled-components';
+
+import content from '../../static/json/about';
+
 const About = () => {
-  return <div>I'm the about section!😎😎 working on the about section</div>;
+  //return <div>I'm the about section!😎😎 working on the about section</div>;
+  return (
+    <>
+      <TitleBlock content={content.titleBlock}></TitleBlock>
+      
+    </>
+  );
 };
+
+/*const PageBody = styled.div`
+  background-color: ${(props) => props.theme.colors.skyBlue};
+`;*/
 
 export default About;
---
+++ src/static/json/about.ts
@@ -0,0 +1,20 @@
+import TeamsTitleImage from '../images/teams_title_block.png';
+
+const aboutContent = {
+    titleBlock: {
+        title: 'About The Club',
+        subtitle: 'We\'re a club at Carleton University that develops pro-bono apps for nonprofits and promotes tech for social good.',
+        image: {
+            picture: TeamsTitleImage,
+            alt: 'about title block picture',
+    },
+    buttons: [
+      {
+        content: 'OUR TEAM >',
+        link: '/teams',
+      },
+    ]
+    },
+}
+
+export default aboutContent
