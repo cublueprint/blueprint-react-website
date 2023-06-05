ea4d0ce88697813e035603ec7817a03823c82c83
heyitsOK <42219936+heyitsOK@users.noreply.github.com>
2023-06-04 20:34:40 -0400

    finished styling for values block

--- src/pages/about/ValuesBlock.tsx
+++ src/pages/about/ValuesBlock.tsx
@@ -16,46 +16,87 @@ const ValuesBlock = (props: ValuesBlockProps) => (
         {props.content.map((value, index) => (
           <ValueDiv>
           <ValueTitle>{value.title}</ValueTitle>
-          <Image src={value.image.picture} alt={value.image.alt}></Image>
-          <Description>{value.description}</Description>
+          <ValueContent>
+            <Description>{value.description}</Description>
+            <Image src={value.image.picture} alt={value.image.alt}></Image>
+          </ValueContent>
           </ValueDiv>
         ))}
     </ValuesBlockDiv>
 );
 
 const ValuesBlockDiv = styled.div`
-    width: 80%;
-    display: grid;
-    
-    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    margin: 30px 220px;
+    & > div:nth-child(even) {
+        > p:first-child {
+            margin-left: auto;
+        }
+        > div {
+            
+        } 
+        > div > p {
+            order: 2;
+            text-align: right;
+        }
+        > div > img {
+            order: 1;
+            margin-left: auto;
+            margin-right: 50px;
+        }
+    }
+
+    @media only screen and (max-width: 1100px) {
+        > div > p:first-child {
+            margin: 0 auto;
+        }
+        > div > div { 
+            flex-direction: column;
+        }
+        > div > div > p {
+            order: 2;
+            text-align: center !important;
+            width: 100%;
+        }
+        > div > div > img {
+            order: 1;
+            margin: 30px auto !important;
+        }
+    } 
 `
 
 const ValueDiv = styled.div`
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-template-rows: auto auto
-    align-items: center;
-    width: 70%;
+    display: flex;
+    flex-direction: column;
+    margin-bottom:30px;
+`
+
+const ValueContent = styled.div`
+    display: flex;
 `
 
 const ValueTitle = styled.p`
   color: blue;
-  grid-column: 1 / 2;
   margin-bottom: 0;
 `
 
 const Image = styled.img`
-    width: 30%;
-    height: auto;
-    margin: 50px;
+    min-width: 20%;
+    max-width: 50%;
+    max-height: 30%;
+    margin-left: 50px;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    order: 2;
 
-    grid-row: 1 / 2;
-    grid-row: span 2;
 `;
 
 const Description = styled.p`
     margin: 0;
     padding: 0;
+    width: 50%;
+    order: 1;
 `;
 
 export default ValuesBlock;
