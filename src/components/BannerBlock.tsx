0bebee6b8a00a4e16d720b0c29766b8ada69ca4b
Musa Ali <musa6ali@gmail.com>
2023-08-14 22:52:08 -0400

    update banner

--- src/components/BannerBlock.tsx
+++ src/components/BannerBlock.tsx
@@ -1,27 +1,46 @@
-import styled from "styled-components";
-import Button from "./Button";
+import styled from 'styled-components';
+import Button from './Button';
+import contactContent from '../static/json/contact';
+import aboutContent from '../static/json/about';
+import { useLocation } from 'react-router-dom';
 
-interface BannerBlockProps {
-    content: {
-        text: string;
-        button: {
-            content: string;
-            link: string;
-        }
-    }
-}
+type Content = {
+  text: string;
+  button: {
+    content: string;
+    link: string;
+  };
+};
 
-const BannerBlock = (props: BannerBlockProps) => (
-    <ApplicationDiv>
-      <FlexContainer>
-        <Text>{props.content.text}</Text>
-      </FlexContainer>
-      <FlexContainer>
-        <Button text={props.content.button.content} link={props.content.button.link}></Button>
-      </FlexContainer>
-    </ApplicationDiv>
-);
+const contentMap: { [key: string]: Content } = {
+  contact: contactContent.banner,
+  about: aboutContent.banner,
+};
 
+const BannerBlock = () => {
+  const location = useLocation();
+  const currentPage = location.pathname.split('/')[1].toLowerCase();
+
+  const content = contentMap[currentPage];
+
+  if (content) {
+    return (
+      <ApplicationDiv>
+        <FlexContainer>
+          <Text>{content.text}</Text>
+        </FlexContainer>
+        <FlexContainer>
+          <Button
+            text={content.button.content}
+            link={content.button.link}
+          ></Button>
+        </FlexContainer>
+      </ApplicationDiv>
+    );
+  }
+
+  return null;
+};
 const ApplicationDiv = styled.div`
   display: flex;
   flex-direction: row;
@@ -41,6 +60,5 @@ const Text = styled.h3`
   text-align: center;
   color: ${(props) => props.theme.colors.primaryBlue};
 `;
-
 
 export default BannerBlock;
