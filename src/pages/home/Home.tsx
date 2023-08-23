3856ec74538fd35c0c550bba3a56ef501853c3fd
AdiChops <aaditya.chopra@carleton.ca>
2023-08-23 14:44:47 -0400

    separated instagram feature

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import TitleBlock from '../../components/TitleBlock';
 import AboutBlock from '../../components/AboutBlock';
 import HomeProjectsBlock from './HomeProjectsBlock';
-import HomeInstagramBlock from './HomeInstagramBlock';
 
 import content from '../../static/json/home';
 
@@ -12,7 +11,6 @@ const Home = () => (
     <AboutBlock content={content.aboutBlock}></AboutBlock>
     <HomeProjectsBlock content={content.projectsBlock}></HomeProjectsBlock>
     <AboutBlock content={content.getInvolvedBlock}></AboutBlock>
-    <HomeInstagramBlock content={content.instagramBlock}></HomeInstagramBlock>
     <AboutBlock content={content.contactUsBlock}></AboutBlock>
   </PageBody>
 );
